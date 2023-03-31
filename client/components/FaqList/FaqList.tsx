32336f4779eed2f54d98f311640bb30fb7061ad8
Andry85 <shapovala@ukr.net>
2023-03-31 13:21:32 +0300

    added faq page

--- client/components/FaqList/FaqList.tsx
+++ client/components/FaqList/FaqList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './FaqList.module.scss';
 
 
@@ -12,9 +12,38 @@ type FaqListProps = {
 
 const FaqList = ({faq} : FaqListProps) => {
 
-    const handleTitleClick = () => {
-        
+    const [faqArray, setFaqArray] = useState(Array(faq.length).fill(0));
+    const arrCopy = [...faqArray];
+
+
+    const handleTitleClick = (index: number): void => {
+        const filterArr = arrCopy.map((el, i) => {
+            if (i === index) {
+                el  = 'active';
+            } else {
+                el  = 0;
+            }
+
+            return el;
+        });
+        setFaqArray(filterArr);
     }
+
+    const handleTitleOnKeyDown = (index: number, event: React.KeyboardEvent<HTMLHeadingElement>): void => {
+        if(event.key === 'Enter') { 
+            const filterArr = arrCopy.map((el, i) => {
+                if (i === index) {
+                    el  = 'active';
+                } else {
+                    el  = 0;
+                }
+    
+                return el;
+            });
+            setFaqArray(filterArr);
+        }
+    }
+
 
     return (
         <>
@@ -23,11 +52,16 @@ const FaqList = ({faq} : FaqListProps) => {
                 <ul className={styles.faqWrapperList}>
                     {faq.map((item, i) => (
                         <li key={i}>
-                            <h3 className={styles.faqWrapperList__title} onClick={handleTitleClick}>
+                            <h3 
+                                className={styles.faqWrapperList__title}
+                                onClick={() => handleTitleClick(i)} 
+                                onKeyDown={(event) => handleTitleOnKeyDown(i, event)}
+                                tabIndex={1}
+                            >
                                 {item.title}
                                 <i className={styles.arrow}></i>
                             </h3>
-                            <div className={styles.faqWrapperList__item}>
+                            <div className={`${styles.faqWrapperList__item} ${faqArray[i] === 'active' ? styles.faqWrapperList__itemActive : ''}`}>
                                 <p>{item.text}</p>
                             </div>
                         </li>
