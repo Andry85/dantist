0ce064d043cee8b0e941bd65040d5c275b78cc6e
Andry85 <shapovala@ukr.net>
2023-05-05 18:07:30 +0300

    fixed path

--- api/index.js
+++ api/index.js
@@ -77,7 +77,7 @@ app.use('/slider', sliderRoute);
 app.use(express.static(path.join(__dirname, '../client/out')));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '..client/out/index.html'));
+    res.sendFile(path.join(__dirname, '../client/out/index.html'));
   });
 
 
